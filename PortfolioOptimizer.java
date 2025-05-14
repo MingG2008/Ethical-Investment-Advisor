872a4f64f15dabb8bbfd309d87b317fb73c94010
MingG2008 <13236@wpsstudent.com>
2025-05-14 12:06:06 -0400

    Update PortfolioOptimizer.java

--- PortfolioOptimizer.java
+++ PortfolioOptimizer.java
@@ -25,16 +25,28 @@ public class PortfolioOptimizer {
         return sum;
     }
 
-    public double calculatePortfolioRisk(double[] weights, List<Company> companies) {
-        // Simplified risk calculation
-        return 0.15; // Example risk of 15%
+    public double calculatePortfolioRisk(double[] weighti, double[] weightj, List<Company> companies) {
+        int sum = 0;
+        Scanner myObj1 = new Scanner(System.in);  
+        System.out.println("Enter volatility of asset i");
+        double volaI = myObj.nextDouble(); 
+        Scanner myObj2 = new Scanner(System.in);  
+        System.out.println("Enter volatility of asset j");
+        double volaJ = myObj.nextDouble(); 
+        Scanner myObj3 = new Scanner(System.in);  
+        System.out.println("Enter correlation between asset i and asset j returns");
+        double corr = myObj.nextDouble(); 
+        for (int i = 0; i < weighti.length; i++){
+            for (int j = 0; j < weightj.length; j++){
+                sum += weighti[i]*weightj[j]*volaI*volaJ*corr;
+            }
+        }
+        return Math.sqrt(sum);
     }
 
     public double[] optimizePortfolio(List<Company> companies, String riskTolerance) {
         int numAssets = companies.size();
         double[] weights = new double[numAssets];
-        
-        // Simple equal weight distribution
         for (int i = 0; i < numAssets; i++) {
             weights[i] = 1.0 / numAssets;
         }
