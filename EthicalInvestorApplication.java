dbad45273f456b9ee85616e99bb2136aa6313ddf
MingG2008 <13236@wpsstudent.com>
2025-05-14 09:01:13 -0400

    Create EthicalInvestorApplication.java

---
+++ EthicalInvestorApplication.java
@@ -0,0 +1,11 @@
+package com.ethicalinvestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthicalInvestorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EthicalInvestorApplication.class, args);
+    }
+}
